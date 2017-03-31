6f4eb4b4d5f3bb5448e5128706bfbc325ca84ddc
jeffreygray <jhg7nm@virginia.edu>
2017-03-31 17:35:19 -0400

    both players can collide with obstacles

--- GameEngine/src/edu/virginia/lab1test/SpaceFights.java
+++ GameEngine/src/edu/virginia/lab1test/SpaceFights.java
@@ -4,20 +4,27 @@ package edu.virginia.lab1test;
 
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 import edu.virginia.engine.controller.GamePad;
 import edu.virginia.engine.display.Game;
 import edu.virginia.engine.display.PhysicsSprite;
 import edu.virginia.engine.display.Sprite;
+import edu.virginia.engine.events.CollisionEvent;
+import edu.virginia.engine.events.CollisionManager;
+import edu.virginia.engine.events.Event;
 import edu.virginia.engine.util.GameClock;
 import edu.virginia.spacefights.classes.Projectile;
 import edu.virginia.spacefights.classes.Ship;
 
 public class SpaceFights extends Game {
-	Sprite scene;
+	Sprite scene, plat1;
 	Ship player1, player2;
 	Sprite p1nrgFront, p1nrgBack, p2nrgFront, p2nrgBack;
+	ArrayList<Sprite> platforms = new ArrayList<Sprite>();
+	CollisionManager collisionManager;
+
 
 	public SpaceFights() {
 		super("Space Fights", 1200, 700);
@@ -32,6 +39,8 @@ public class SpaceFights extends Game {
 		p2nrgFront = new Sprite("nrgFront", "frontNRG.png");
 		p2nrgBack = new Sprite("nrgBack", "rearNRG.png");
 
+		plat1 = new Sprite("plat1", "platformSpace.png");
+		collisionManager = new CollisionManager();
 		
 		player1.setPosition(800, 350);
 		player1.setScaleX(0.8);
@@ -55,6 +64,21 @@ public class SpaceFights extends Game {
 		p2nrgBack.addChild(p2nrgFront);
 		scene.addChild(player1);
 		scene.addChild(player2);
+		scene.addChild(plat1);;
+	
+		
+		plat1.setParent(scene);
+
+		
+		plat1.setPosition(300, 350);;
+
+		
+		platforms.add(plat1);
+		
+		player1.addEventListener(collisionManager, CollisionEvent.PLATFORM);
+		player2.addEventListener(collisionManager, CollisionEvent.PLATFORM);
+
+
 	}
 
 	@Override
@@ -83,6 +107,69 @@ public class SpaceFights extends Game {
 			else if(shipPos.y > 700-player1.getHeight()) {
 				player1.setYPosition(700-player1.getHeight());
 				player1.setYv(-player1.getYv());
+			}
+			
+			// collisions p1
+			for(Sprite plat: platforms) {
+				if(player1.collidesWith(plat)) {
+					player1.dispatchEvent(new Event(CollisionEvent.PLATFORM, player1));
+					
+					Rectangle mHB = player1.getHitbox();
+					Rectangle pHB = plat.getHitbox();
+					Rectangle overlap = mHB.intersection(pHB);
+					
+					if(overlap.width < overlap.height) {
+						// coming from side; which side?
+						if(mHB.x > pHB.x) {  // collision with right side of platform
+							if(player1.getScaleX() > 0) // facing right
+								player1.setPosition(pHB.getX() + pHB.getWidth(), player1.getPosition().y);
+							else
+								player1.setPosition(pHB.getX() + pHB.getWidth() + mHB.getWidth(), player1.getPosition().y);
+							
+						}
+						else player1.setPosition(pHB.getX() - mHB.getWidth(), player1.getPosition().y); // on left side
+						System.out.println("LR WALL HIT");
+						player1.setXv(0);
+					} else {
+						// coming from top or bottom
+						if(mHB.y < pHB.y) {
+							player1.setPosition(player1.getPosition().x, pHB.getY() - mHB.getHeight()); //above
+						}
+						else player1.setPosition(player1.getPosition().x, pHB.getY() + pHB.getHeight()); // below
+						player1.setYv(0);
+					}
+				}
+			}
+			
+			for(Sprite plat: platforms) { // p2 collisions
+				if(player2.collidesWith(plat)) {
+					player2.dispatchEvent(new Event(CollisionEvent.PLATFORM, player2));
+					
+					Rectangle mHB = player2.getHitbox();
+					Rectangle pHB = plat.getHitbox();
+					Rectangle overlap = mHB.intersection(pHB);
+					
+					if(overlap.width < overlap.height) {
+						// coming from side; which side?
+						if(mHB.x > pHB.x) {  // collision with right side of platform
+							if(player2.getScaleX() > 0) // facing right
+								player2.setPosition(pHB.getX() + pHB.getWidth(), player2.getPosition().y);
+							else
+								player2.setPosition(pHB.getX() + pHB.getWidth() + mHB.getWidth(), player1.getPosition().y);
+							
+						}
+						else player2.setPosition(pHB.getX() - mHB.getWidth(), player2.getPosition().y); // on left side
+						System.out.println("LR WALL HIT");
+						player2.setXv(0);
+					} else {
+						// coming from top or bottom
+						if(mHB.y < pHB.y) {
+							player2.setPosition(player2.getPosition().x, pHB.getY() - mHB.getHeight()); //above
+						}
+						else player2.setPosition(player2.getPosition().x, pHB.getY() + pHB.getHeight()); // below
+						player2.setYv(0);
+					}
+				}
 			}
 
 			p1nrgBack.setPosition(shipPos.x-player1.getWidth()/2, shipPos.y-player1.getHeight()/3);
