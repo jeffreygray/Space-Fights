d3591dabc22f73000ab2556b8fc7c467de22dbeb
jeffreygray <jhg7nm@virginia.edu>
2017-04-09 17:40:11 -0400

    adding some more level attributes
    pew pew!

--- GameEngine/src/edu/virginia/lab1test/SpaceFights.java
+++ GameEngine/src/edu/virginia/lab1test/SpaceFights.java
@@ -24,7 +24,7 @@ public class SpaceFights extends Game {
 	static int gameWidth = 1800;
 	static int gameHeight = 1000;
 	static double dampen = -0.65;
-	Sprite scene, plat1, plat2, plat3, plat4, plat5;
+	Sprite scene, plat1, plat2, plat3, plat4, plat5, plat6, plat7, plat8, plat9, plat10, plat11, plat12, plat13, plat14, plat15;
 	Ship player1, player2, player3;
 //	Sprite p1nrgFront, p1nrgBack, p2nrgFront, p2nrgBack;
 	ArrayList<Sprite> platforms = new ArrayList<Sprite>();
@@ -43,7 +43,7 @@ public class SpaceFights extends Game {
 		players.add(player2);
 //		players.add(player3);
 		
-		scene = new Sprite("scene", "backgroundSF.png");
+		scene = new Sprite("scene", "background.png");
 		scene.setScaleX(1);
 
 /*		p1nrgFront = new Sprite("nrgFront", "frontNRG.png");
@@ -57,14 +57,41 @@ public class SpaceFights extends Game {
 		plat2.setScaleX(9);
 		plat2.setScaleY(9);
 		plat3 = new Sprite("plat3", "platformSpaceVertical.png");
-//		plat4 = new Sprite("plat4", "platformSmall.png");
-//		plat5 = new Sprite("plat5", "platformSmall.png");
+		plat4 = new Sprite("plat4", "platformSpace.png");
+		plat5 = new Sprite("plat5", "platformSpace.png");
+		plat6 = new Sprite("plat6", "platformSpace.png");
+		plat7 = new Sprite("plat7", "platformSpace.png");
+		plat8 = new Sprite("plat8", "platformSpace.png");
+		plat9 = new Sprite("plat9", "platformSpace.png");
+		plat10 = new Sprite("plat10", "platformSpace.png");
+		plat11 = new Sprite("plat11", "platformSpace.png");
+		plat12 = new Sprite("plat12", "platformSpace.png");
+		plat13 = new Sprite("plat13", "platformSpaceVertical.png");
+		plat14 = new Sprite("plat14", "platformSpace.png");
+		plat15 = new Sprite("plat15", "platformSpaceVertical.png");
+
+
+
+
+
 
 		platforms.add(plat1);
      	platforms.add(plat2);
 		platforms.add(plat3);
-//		platforms.add(plat4);
-//		platforms.add(plat5);
+		platforms.add(plat4);
+		platforms.add(plat5);
+		platforms.add(plat6);
+		platforms.add(plat7);
+		platforms.add(plat8);
+		platforms.add(plat9);
+		platforms.add(plat10);
+		platforms.add(plat11);
+		platforms.add(plat12);
+		platforms.add(plat13);
+		platforms.add(plat14);
+		platforms.add(plat15);
+
+		
 
 		
 		collisionManager = new CollisionManager();
@@ -100,6 +127,30 @@ public class SpaceFights extends Game {
 		plat1.setPosition(400, 250);
 		plat2.setPosition(900, 300);
 		plat3.setPosition(1350, 470);
+		plat4.setPosition(0, 920);
+		plat5.setPosition(plat4.getWidth(), 925);
+		plat6.setPosition(plat4.getWidth() * 2, 925);
+		plat7.setPosition(plat4.getWidth() * 3, 925);
+		plat8.setPosition(plat4.getWidth() * 4, 925);
+		plat9.setPosition(plat4.getWidth() * 5, 925);
+		plat10.setPosition(plat4.getWidth() * 6, 925);
+		plat11.setPosition(plat4.getWidth() * 7, 925);
+		plat12.setPosition(plat4.getWidth() * 8, 925);
+		plat13.setPosition(0,0);
+		plat13.setScaleY(10);
+		plat14.setPosition(1,0);
+		plat14.setScaleX(10);
+		plat15.setPosition(gameWidth - plat15.getWidth(), 0);
+		plat15.setScaleY(10);
+
+		
+	
+
+
+
+
+
+
 
 //		plat3.setPosition(300, 348 +  2*plat2.getHeight());
 //		plat4.setPosition(300, 347 + 3*plat2.getHeight());
@@ -133,14 +184,14 @@ public class SpaceFights extends Game {
 //			p2nrgFront.setScaleX((double) player2.getNrg() / player2.getShipType().getNrgCap());
 			for(Ship player: players) {
 				Point shipPos = player.getPosition();
-				if (shipPos.x <= 0) {
+				if (shipPos.x < 0) {
 					player.setXPosition(0);
 					player.setXv(-player1.getXv());
 				} else if (shipPos.x > gameWidth - player.getWidth()) {
 					player.setXPosition(gameWidth - player.getWidth());
 					player.setXv(dampen * player1.getXv());
 				}
-				if (shipPos.y <= 0) {
+				if (shipPos.y < 0) {
 					player.setYPosition(0);
 					player.setYv(dampen * player.getYv());
 				} else if (shipPos.y > gameHeight - player.getHeight()) {
